ce31a226b4e12d5dc545388019934a8605ed77ff
SailorSnoW <snxwin@pm.me>
2023-02-28 02:17:24 +0100

    add keystore injection script

---
+++ scripts/insert_keys.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# $SECRET must include derivation path if any.
+
+./target/release/allfeat key insert --base-path "$NODE_PATH" --chain ./node/src/chain_specs/symphonie.json --scheme Ed25519 --suri "$SECRET//grandpa" --key-type gran
+./target/release/allfeat key insert --base-path "$NODE_PATH" --chain ./node/src/chain_specs/symphonie.json --scheme Sr25519 --suri "$SECRET//babe" --key-type babe
+./target/release/allfeat key insert --base-path "$NODE_PATH" --chain ./node/src/chain_specs/symphonie.json --scheme Sr25519 --suri "$SECRET//im_online" --key-type imon
+./target/release/allfeat key insert --base-path "$NODE_PATH" --chain ./node/src/chain_specs/symphonie.json --scheme Sr25519 --suri "$SECRET//authority_discovery" --key-type audi
+
+printf "Success."
