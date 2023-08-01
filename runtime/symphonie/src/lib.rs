fe1f338ba3d4fe9e971de881e8a3d751da390f1c
SailorSnoW <sailorsnow@pm.me>
2023-08-01 18:08:58 +0200

    bump runtime version

--- runtime/symphonie/src/lib.rs
+++ runtime/symphonie/src/lib.rs
@@ -129,10 +129,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 100,
+	spec_version: 101,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 3,
 	state_version: 1,
 };
 
