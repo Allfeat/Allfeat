3772e70970fa570226136cd1b30b174034197695
SailorSnoW <sailorsnow@pm.me>
2023-08-18 14:57:13 +0200

    fix try-runtime build

--- node/src/command.rs
+++ node/src/command.rs
@@ -31,6 +31,12 @@ use sp_keyring::Sr25519Keyring;
 use std::sync::Arc;
 use symphonie_runtime::Block;
 
+#[cfg(feature = "try-runtime")]
+use {
+	crate::service::ExecutorDispatch, symphonie_runtime::constants::time::SLOT_DURATION,
+	try_runtime_cli::block_building_info::substrate_info,
+};
+
 impl SubstrateCli for Cli {
 	fn impl_name() -> String {
 		"🎶 Allfeat Node".into()
@@ -197,6 +203,7 @@ pub fn run() -> Result<()> {
 		},
 		#[cfg(feature = "try-runtime")]
 		Some(Subcommand::TryRuntime(cmd)) => {
+			use sc_executor::{sp_wasm_interface::ExtendedHostFunctions, NativeExecutionDispatch};
 			let runner = cli.create_runner(cmd)?;
 			runner.async_run(|config| {
 				// we don't need any of the components of new_partial, just a runtime, or a task
@@ -206,7 +213,15 @@ pub fn run() -> Result<()> {
 					sc_service::TaskManager::new(config.tokio_handle.clone(), registry)
 						.map_err(|e| sc_cli::Error::Service(sc_service::Error::Prometheus(e)))?;
 
-				Ok((cmd.run::<Block, ExecutorDispatch>(config), task_manager))
+				let info_provider = substrate_info(SLOT_DURATION);
+
+				Ok((
+					cmd.run::<Block, ExtendedHostFunctions<
+						sp_io::SubstrateHostFunctions,
+						<ExecutorDispatch as NativeExecutionDispatch>::ExtendHostFunctions,
+					>, _>(Some(info_provider)),
+					task_manager,
+				))
 			})
 		},
 		#[cfg(not(feature = "try-runtime"))]
