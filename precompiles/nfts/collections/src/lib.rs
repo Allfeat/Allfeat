9d4270122682b65bbe7248af49b815a41988a7af
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2024-03-25 17:54:34 -0400

    wip: add ntf collection precompile testing

--- precompiles/nfts/collections/src/lib.rs
+++ precompiles/nfts/collections/src/lib.rs
@@ -140,8 +140,8 @@ where
 		// Storage item: Asset:
 		// Blake2_128(16) + CollectionId + CollectionDetails
 		handle.record_db_read::<Runtime>(
-			16 + CollectionIdOf::<Runtime>::max_encoded_len() +
-				CollectionDetailsOf::<Runtime>::max_encoded_len(),
+			16 + CollectionIdOf::<Runtime>::max_encoded_len()
+				+ CollectionDetailsOf::<Runtime>::max_encoded_len(),
 		)?;
 
 		let collection_details = pallet_nfts::Collection::<Runtime>::get(collection_id)
@@ -151,7 +151,6 @@ where
 	}
 
 	// Extrinsics
-
 	#[precompile::public("mint(uint256,address,(bool,((bool,uint256),(bool,uint256))))")]
 	fn mint(
 		collection_id: CollectionIdOf<Runtime>,
