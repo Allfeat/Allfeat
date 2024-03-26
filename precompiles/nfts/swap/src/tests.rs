478f28bc7efc616308394d7f7ef661c446dd1448
mE0w <romain.panno@epitech.eu>
2024-03-27 00:28:56 +0100

    Tests/pallet evm precompile nfts swap (#22)
    * add tests for precompiles nfts swap
    
    that includes 3 tests:
    - create_swap_works
    - cancel_swap_works
    - claim_swap_works
    
    * fix missings checks
    
    adding checks to ensure that the create swap function is working
    addming more checks and using pallet_nfts, when necessary
    
    * add asserts for swap content
    
    adding some asserts to test the content of PendingSwapOf in the
    create_swap_works func

--- precompiles/nfts/swap/src/tests.rs
+++ precompiles/nfts/swap/src/tests.rs
@@ -17,11 +17,43 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use crate::{mock::*, NftsSwapPrecompileCall};
+use pallet_evm_precompile_nfts_tests::{ExtBuilder, ALICE, BOB};
+use precompile_utils::testing::*;
+use sp_core::U256;
+use pallet_evm_precompile_nfts_types::solidity::{OptionalU256,OptionalPriceWithDirection, PriceWithDirection, PriceDirection};
+use pallet_nfts::Pallet as NftsPallet;
+use frame_support::assert_ok;
+use frame_system::pallet_prelude::OriginFor;
 
 type PCall = NftsSwapPrecompileCall<Runtime>;
 
-fn _precompiles() -> Precompiles<Runtime> {
+fn precompiles() -> Precompiles<Runtime> {
 	PrecompilesValue::get()
+}
+
+fn mint_each_collections() {
+	assert_ok!(NftsPallet::<Runtime>::mint(
+		OriginFor::<Runtime>::signed(ALICE.into()),
+		0,
+		1,
+		ALICE.into(),
+		None
+	));
+	assert_ok!(NftsPallet::<Runtime>::mint(
+		OriginFor::<Runtime>::signed(BOB.into()),
+		1,
+		1,
+		BOB.into(),
+		None
+	));
+	assert_eq!(get_owner_of_item(0, 1), Some(ALICE.into()));
+	assert_eq!(get_owner_of_item(1, 1), Some(BOB.into()));
+}
+
+fn get_owner_of_item(collection_id: u128, item_id: u128) -> Option<AccountId> {
+	let owner_id: Option<AccountId>  = NftsPallet::<Runtime>::owner(collection_id, item_id);
+
+	owner_id
 }
 
 #[test]
@@ -33,15 +65,124 @@ fn selectors() {
 
 #[test]
 fn create_swap_works() {
-	todo!()
+	ExtBuilder::<Runtime>::default()
+        .with_balances(vec![(ALICE.into(), 1000), (BOB.into(), 1000)])
+        .build_with_collections()
+        .execute_with(|| {
+			let collection_id = 0;
+			let item_id = 1;
+			let desired_collection: u128 = 1;
+			let maybe_desired_item = OptionalU256::from(Some(U256::from(1)));
+			let maybe_price = OptionalPriceWithDirection::from(Some(PriceWithDirection::new(U256::from(100), PriceDirection::Receive)));
+			let duration = 100;
+			let expect_deadline = 101;
+
+			mint_each_collections();
+			assert_eq!(pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id).is_none(), true);
+			precompiles().prepare_test(
+				ALICE,
+				Precompile1,
+				PCall::create_swap {
+					offered_collection: collection_id.into(),
+					offered_item: item_id.into(),
+					desired_collection: desired_collection.clone().into(),
+					maybe_desired_item: maybe_desired_item.clone(),
+					maybe_price: maybe_price.clone(),
+					duration: duration.clone().into(),
+				},
+			).execute_returns(true);
+
+			assert_eq!(get_owner_of_item(0, 1), Some(ALICE.into())); // check that the item is still owned by ALICE
+
+			let swap = pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id);
+			assert_eq!(swap.is_some(), true);
+
+			let swap = swap.unwrap();
+			assert_eq!(*swap.desired_collection(), desired_collection);
+			assert_eq!(swap.desired_item().unwrap(), maybe_desired_item.value.try_into().unwrap());
+			assert_eq!(swap.price().as_ref().unwrap(), &maybe_price.value.try_into().unwrap());
+			assert_eq!(*swap.deadline(), expect_deadline);
+
+        });
 }
 
 #[test]
 fn cancel_swap_works() {
-	todo!()
+	ExtBuilder::<Runtime>::default()
+        .with_balances(vec![(ALICE.into(), 1000), (BOB.into(), 1000)])
+        .build_with_collections()
+        .execute_with(|| {
+			let collection_id = 0;
+			let item_id = 1;
+			let desired_collection = 1;
+			let maybe_desired_item = Some(1);
+			let maybe_price = OptionalPriceWithDirection::from(Some(PriceWithDirection::new(U256::from(100), PriceDirection::Receive)));
+			let duration = 100;
+
+			mint_each_collections();
+			assert_eq!(pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id).is_none(), true);
+			assert_ok!(NftsPallet::<Runtime>::create_swap(
+				OriginFor::<Runtime>::signed(ALICE.into()),
+				collection_id,
+				item_id,
+				desired_collection,
+				maybe_desired_item,
+				maybe_price.try_into().unwrap(),
+				duration
+			));
+			assert_eq!(pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id).is_some(), true);
+
+			precompiles().prepare_test(
+				ALICE,
+				Precompile1,
+				PCall::cancel_swap {
+					offered_collection: 0.into(),
+					offered_item: 1.into(),
+				},
+			).execute_returns(true);
+
+			assert_eq!(pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id).is_none(), true);
+			assert_eq!(get_owner_of_item(0, 1), Some(ALICE.into()));
+        });
 }
 
 #[test]
 fn claim_swap_works() {
-	todo!()
+	ExtBuilder::<Runtime>::default()
+		.with_balances(vec![(ALICE.into(), 1000), (BOB.into(), 1000)])
+		.build_with_collections()
+		.execute_with(|| {
+			let collection_id = 0;
+			let item_id = 1;
+			let desired_collection = 1;
+			let maybe_desired_item = Some(1);
+			let maybe_price = OptionalPriceWithDirection::from(Some(PriceWithDirection::new(U256::from(100), PriceDirection::Receive)));
+			let duration = 100;
+
+			mint_each_collections();
+			assert_ok!(NftsPallet::<Runtime>::create_swap(
+				OriginFor::<Runtime>::signed(ALICE.into()),
+				collection_id,
+				item_id,
+				desired_collection,
+				maybe_desired_item,
+				maybe_price.try_into().unwrap(),
+				duration
+			));
+
+			precompiles().prepare_test(
+				BOB,
+				Precompile1,
+				PCall::claim_swap {
+					send_collection: 1.into(),
+					send_item: 1.into(),
+					receive_collection: 0.into(),
+					receive_item: 1.into(),
+					witness_price: OptionalPriceWithDirection::from(Some(PriceWithDirection::new(U256::from(100), PriceDirection::Receive))),
+				},
+			).execute_returns(true);
+
+			assert_eq!(pallet_nfts::PendingSwapOf::<Runtime>::get(collection_id, item_id).is_none(), true);
+			assert_eq!(get_owner_of_item(0, 1), Some(BOB.into()));
+		});
 }
