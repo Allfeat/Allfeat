b0004e5e8cfd9d1715e19ee80c9c1dbfc9f4fce8
SailorSnoW <sailorsnow@pm.me>
2024-08-09 22:04:52 +0200

    bump spec_version

--- runtime/harmonie/src/lib.rs
+++ runtime/harmonie/src/lib.rs
@@ -71,7 +71,7 @@ pub const VERSION: sp_version::RuntimeVersion = sp_version::RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 220,
+	spec_version: 230,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
