9947329b2a1e13cb0c18a19e4f3c5485dd14f000
SailorSnoW <sailorsnow@pm.me>
2024-08-12 23:33:21 +0200

    remove NPoS logics

--- runtime/harmonie/src/lib.rs
+++ runtime/harmonie/src/lib.rs
@@ -71,10 +71,10 @@ pub const VERSION: sp_version::RuntimeVersion = sp_version::RuntimeVersion {
 	// and set impl_version to 0. If only runtime
 	// implementation changes and behavior does not, then leave spec_version as
 	// is and increment impl_version.
-	spec_version: 230,
+	spec_version: 300,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 0,
 };
 
