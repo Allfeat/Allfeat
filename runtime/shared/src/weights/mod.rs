ba915f71a650397b8cd04d837b6fe9306d1a615f
SailorSnoW <sailorsnow@pm.me>
2024-08-13 02:16:38 +0200

    removed unused stuff

--- runtime/shared/src/weights/mod.rs
+++ runtime/shared/src/weights/mod.rs
@@ -18,13 +18,11 @@
 
 // pub mod artists; TODO
 pub mod balances;
-pub mod election_provider_multi_phase;
 pub mod evm;
 pub mod system;
 // pub mod identity;
 pub mod im_online;
 pub mod multisig;
-pub mod nomination_pools;
 pub mod preimage;
 pub mod proxy;
 pub mod scheduler;
