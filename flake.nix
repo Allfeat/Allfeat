64c3d6171b6574a3395113c2045b95477de7da4c
SailorSnoW <sailorsnow@pm.me>
2025-02-26 17:45:15 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,29 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
-  }: let
-    overlays = [
-      rust-overlay.overlays.default
-      (final: prev: {
-        rustToolchain =
-          prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      })
-    ];
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems
-      (system: f {pkgs = import nixpkgs {inherit overlays system;};});
-  in {
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs;
-          [rustToolchain openssl protobuf clang pkg-config rust-analyzer]
-          ++ lib.optional
-          (system == "x86_64-darwin" || system == "aarch64-darwin")
-          darwin.apple_sdk.frameworks.SystemConfiguration;
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustup
+          clang
+          protobuf
+          rust-jemalloc-sys-unprefixed
+          pkg-config
+          openssl
+        ];
 
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
       };
     });
-  };
 }
