a2bf2ed12a3866944aedc0fbf128497787fb19bd
SailorSnoW <sailorsnow@pm.me>
2025-02-26 16:47:41 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -7,31 +7,33 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      overlays = [
-        rust-overlay.overlays.default
-        (final: prev: {
-          rustToolchain =
-            prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        })
-      ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            [ rustToolchain openssl protobuf clang pkg-config rust-analyzer ]
-            ++ lib.optional
-            (system == "x86_64-darwin" || system == "aarch64-darwin")
-            darwin.apple_sdk.frameworks.SystemConfiguration;
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    overlays = [
+      rust-overlay.overlays.default
+      (final: prev: {
+        rustToolchain =
+          prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems
+      (system: f {pkgs = import nixpkgs {inherit overlays system;};});
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs;
+          [rustToolchain openssl protobuf clang pkg-config rust-analyzer]
+          ++ lib.optional
+          (system == "x86_64-darwin" || system == "aarch64-darwin")
+          darwin.apple_sdk.frameworks.SystemConfiguration;
 
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-        };
-      });
-    };
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+      };
+    });
+  };
 }
