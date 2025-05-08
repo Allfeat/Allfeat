1e87966d59b4d6b732548db4abdb4871c993f0be
SailorSnoW <sailorsnow@pm.me>
2025-05-08 20:32:47 +0200

    add just to flake

--- flake.nix
+++ flake.nix
@@ -7,36 +7,43 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs;
-          [
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-            clang
-            protobuf
-            openssl
-            pkg-config
-          ]
-          ++ lib.optionals stdenv.hostPlatform.isLinux [rust-jemalloc-sys-unprefixed]
-          ++ lib.optionals stdenv.hostPlatform.isDarwin [
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+              clang
+              protobuf
+              openssl
+              pkg-config
+              just
+            ]
+            ++ lib.optionals stdenv.hostPlatform.isLinux [ rust-jemalloc-sys-unprefixed ]
+            ++ lib.optionals stdenv.hostPlatform.isDarwin [
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
 
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-        # New flag required since https://github.com/eigerco/polka-storage/pull/730
-        CRATE_CC_NO_DEFAULTS = 1;
-      };
-    });
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          # New flag required since https://github.com/eigerco/polka-storage/pull/730
+          CRATE_CC_NO_DEFAULTS = 1;
+        };
+      }
+    );
 }
