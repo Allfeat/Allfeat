6793b59db34759e44518a0ead14f7e930738c6f2
SailorSnoW <sailorsnow@pm.me>
2025-06-07 16:07:42 +0200

    separate runtime api in another module

--- runtime/melodie/src/pallets/aura.rs
+++ runtime/melodie/src/pallets/aura.rs
@@ -18,7 +18,7 @@
 
 use crate::*;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_core::ConstBool;
+use sp_core::{ConstBool, ConstU32};
 
 impl pallet_aura::Config for Runtime {
     type AuthorityId = AuraId;
