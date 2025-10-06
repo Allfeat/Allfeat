db913e018f470d2746464d8be6e14ecc4dc2d6cd
miquelcabot <miquel.cabot@gmail.com>
2025-10-06 22:53:02 +0200

    refactor ATS pallet: enhance zero-knowledge proof verification

--- pallets/ats/src/lib.rs
+++ pallets/ats/src/lib.rs
@@ -31,9 +31,14 @@ mod benchmarking;
 
 extern crate alloc;
 
+use ark_bn254::{Bn254, Fr};
+use ark_ff::{BigInteger, PrimeField};
+use ark_groth16::{Groth16, PreparedVerifyingKey, Proof, VerifyingKey};
+use ark_serialize::{CanonicalDeserialize, SerializationError};
 use frame_support::{pallet_prelude::*, sp_runtime::Saturating};
 use frame_system::pallet_prelude::*;
 pub use pallet::*;
+use scale_info::prelude::vec::Vec;
 use sp_core::U256;
 
 #[frame_support::pallet()]
@@ -133,7 +138,7 @@ pub mod pallet {
     #[codec(mel_bound(T: Config))]
     pub struct AtsData<T: Config> {
         pub owner: T::AccountId,
-		pub hash_commitment: Hash256,
+        pub hash_commitment: Hash256,
         pub timestamp: Moment,
     }
 
@@ -154,7 +159,8 @@ pub mod pallet {
     pub type AtsOf<T: Config> = StorageMap<_, Blake2_128Concat, Hash256, AtsData<T>>;
 
     #[pallet::storage]
-	pub type AtsByOwner<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, BoundedVec<Hash256, ConstU32<1000>>>;
+    pub type AtsByOwner<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::AccountId, BoundedVec<Hash256, ConstU32<1000>>>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -164,11 +170,11 @@ pub mod pallet {
             hash_commitment: Hash256,
             data_collateral: BalanceOf<T>,
         },
-		ATSClaimed {
-			old_owner: T::AccountId,
-			new_owner: T::AccountId,
-			hash_commitment: Hash256,
-		},
+        ATSClaimed {
+            old_owner: T::AccountId,
+            new_owner: T::AccountId,
+            hash_commitment: Hash256,
+        },
     }
 
     #[pallet::error]
@@ -181,6 +187,10 @@ pub mod pallet {
         CantHoldFunds,
         /// The owner has reached the maximum number of ATS entries.
         MaxAtsPerOwnerReached,
+        /// Serialization or deserialization failed
+        InvalidData,
+        /// Verification failed
+        VerificationFailed,
     }
 
     #[pallet::call(weight(<T as Config>::WeightInfo))]
@@ -190,11 +200,26 @@ pub mod pallet {
     {
         #[pallet::call_index(0)]
         #[pallet::weight(T::WeightInfo::register(hash_commitment.encoded_size() as u32))]
-        pub fn register(origin: OriginFor<T>, hash_commitment: Hash256) -> DispatchResult {
+        pub fn register(
+            origin: OriginFor<T>,
+            hash_commitment: Hash256,
+            vk: Vec<u8>,
+            pubs: Vec<[u8; 32]>,
+            proof: Vec<u8>,
+        ) -> DispatchResult {
             let sender = T::ProviderOrigin::ensure_origin(origin)?;
 
+            // Verify the zero-knowledge proof
+            ensure!(
+                Self::verify_zkp(vk, pubs, proof)?,
+                Error::<T>::VerificationFailed
+            );
+
             // Check if ATS with this hash commitment already exists
-            ensure!(!AtsOf::<T>::contains_key(hash_commitment), Error::<T>::AtsDataAlreadyExist);
+            ensure!(
+                !AtsOf::<T>::contains_key(hash_commitment),
+                Error::<T>::AtsDataAlreadyExist
+            );
 
             // Get current timestamp
             let timestamp = T::Timestamp::now();
@@ -206,14 +231,11 @@ pub mod pallet {
                 timestamp,
             };
             let size = ats_data.encoded_size() as u32;
-			let data_cost = Self::calculate_ats_colateral(size);
+            let data_cost = Self::calculate_ats_colateral(size);
 
             // Hold the deposit from the sender
-            T::Currency::hold(
-                &HoldReason::AtsRegistration.into(),
-                &sender,
-                data_cost,
-            ).map_err(|_| Error::<T>::CantHoldFunds)?;
+            T::Currency::hold(&HoldReason::AtsRegistration.into(), &sender, data_cost)
+                .map_err(|_| Error::<T>::CantHoldFunds)?;
 
             // Store ATS data
             AtsOf::<T>::insert(hash_commitment, ats_data);
@@ -238,12 +260,23 @@ pub mod pallet {
 
         #[pallet::call_index(2)]
         #[pallet::weight(T::WeightInfo::claim())]
-        pub fn claim(origin: OriginFor<T>, hash_commitment: Hash256) -> DispatchResult {
+        pub fn claim(
+            origin: OriginFor<T>,
+            hash_commitment: Hash256,
+            vk: Vec<u8>,
+            pubs: Vec<[u8; 32]>,
+            proof: Vec<u8>,
+        ) -> DispatchResult {
             let sender = T::ProviderOrigin::ensure_origin(origin)?;
 
+            // Verify the zero-knowledge proof
+            ensure!(
+                Self::verify_zkp(vk, pubs, proof)?,
+                Error::<T>::VerificationFailed
+            );
+
             // Get the ATS data
-            let mut ats_data = AtsOf::<T>::get(hash_commitment)
-                .ok_or(Error::<T>::AtsNotFound)?;
+            let mut ats_data = AtsOf::<T>::get(hash_commitment).ok_or(Error::<T>::AtsNotFound)?;
 
             let old_owner = ats_data.owner.clone();
 
@@ -266,12 +299,12 @@ pub mod pallet {
                 Ok(())
             })?;
 
-			// Emit event
-			Self::deposit_event(Event::ATSClaimed {
-				old_owner,
-				new_owner: sender,
-				hash_commitment,
-			});
+            // Emit event
+            Self::deposit_event(Event::ATSClaimed {
+                old_owner,
+                new_owner: sender,
+                hash_commitment,
+            });
 
             Ok(())
         }
@@ -281,5 +314,48 @@ pub mod pallet {
 impl<T: Config> Pallet<T> {
     fn calculate_ats_colateral(size: u32) -> types::BalanceOf<T> {
         T::ByteDepositCost::get().saturating_mul(types::BalanceOf::<T>::from(size))
+    }
+
+    fn fr_from_be32(bytes: &[u8; 32]) -> Result<Fr, SerializationError> {
+        // reduce mod p from BE bytes
+        let x = Fr::from_be_bytes_mod_order(bytes);
+
+        // re-encode canonically (minimal BE) and compare with input
+        let be_min = x.into_bigint().to_bytes_be();
+        if be_min.len() > 32 {
+            return Err(SerializationError::InvalidData);
+        }
+        let mut be32 = [0u8; 32];
+        be32[32 - be_min.len()..].copy_from_slice(&be_min);
+
+        if &be32 == bytes {
+            Ok(x)
+        } else {
+            Err(SerializationError::InvalidData)
+        }
+    }
+
+    fn verify_zkp(vk: Vec<u8>, pubs: Vec<[u8; 32]>, proof: Vec<u8>) -> Result<bool, Error<T>> {
+        // 1) Deserialize
+        let vk = VerifyingKey::<Bn254>::deserialize_compressed(vk.as_slice())
+            .map_err(|_| Error::<T>::InvalidData)?;
+        let pvk: PreparedVerifyingKey<Bn254> = ark_groth16::prepare_verifying_key(&vk);
+
+        let proof = Proof::<Bn254>::deserialize_compressed(proof.as_slice())
+            .map_err(|_| Error::<T>::InvalidData)?;
+
+        let mut publics: Vec<Fr> = Vec::with_capacity(pubs.len());
+        for b in &pubs {
+            publics.push(Self::fr_from_be32(b).map_err(|_| Error::<T>::InvalidData)?);
+        }
+
+        // 2) Verify
+        let proof_ok = match Groth16::<Bn254>::verify_proof(&pvk, &proof, &publics) {
+            Ok(true) => true,
+            Ok(false) => false,
+            Err(_) => false,
+        };
+
+        Ok(proof_ok)
     }
 }
